2db79248d707c32cd522b9138097b9b8c91581dd
Mahdi <mahdi.talaee1379@gmail.com>
2024-12-07 22:11:57 +0330

    Fix - We don't need to do assertion here as this is client side!

--- src/EventHandler/Filter/FilterSingleCommand.php
+++ src/EventHandler/Filter/FilterSingleCommand.php
@@ -44,8 +44,7 @@ class FilterSingleCommand extends Filter
     public function apply(Update $update): bool
     {
         if($update instanceof Message) {
-            Assert::true(preg_match("/^\/(\w+)@([a-zA-Z](?:[a-zA-Z0-9]*(?:_[a-zA-Z0-9]+)?)*)$/", $update->message, $matches) === 1, "An invalid command was specified!");
-            Assert::true($matches[2] >= 3 && $matches[2] <= 32, "Username must contain between 3 and 32 characters!");
+            preg_match("/^\/(\w+)@([a-zA-Z](?:[a-zA-Z0-9]*(?:_[a-zA-Z0-9]+)?)*)$/", $update->message, $matches);
             if(\in_array($matches[2], $this->usernames, true)) {
                 return (new FilterCommand($this->command, [CommandType::SLASH]))->apply($update);
             }
