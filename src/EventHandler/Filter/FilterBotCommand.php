77db54932d409e5ed791aa74da03bde107d61d66
Daniil Gentili <daniil@daniil.it>
2024-12-13 12:19:28 +0100

    Update FilterBotCommand.php

--- src/EventHandler/Filter/FilterBotCommand.php
+++ src/EventHandler/Filter/FilterBotCommand.php
@@ -34,10 +34,10 @@ class FilterBotCommand extends Filter
     public function initialize(EventHandler $API): Filter
     {
         Assert::true($API->isSelfBot(), 'This filter can only be used by bots!');
-        foreach ($API->getInfo('me', API::INFO_TYPE_USERNAMES) as $_) {
-            $filters[] = new FilterCommand($this->command, [CommandType::SLASH]);
+        $filters = [new FilterCommand($this->command, [CommandType::SLASH])];
+        foreach ($API->getInfo('me', API::INFO_TYPE_USERNAMES) as $username) {
+            $filters[] = new FilterCommand("{$this->command}@$username", [CommandType::SLASH]);
         }
-        Assert::notEmpty($filters ?? [], 'Filter commands must not be empty.');
         return new FiltersOr(...$filters);
     }
     public function __construct(private readonly string $command)
