9f3de196cd8f5fd97daf3be511ef86ca2c019122
Alexander Pankratov <s.pankratoff@gmail.com>
2024-12-18 11:44:54 +0100

    Fix: ads

--- src/Wrappers/Ads.php
+++ src/Wrappers/Ads.php
@@ -46,7 +46,7 @@ trait Ads
         if ($cache && $cache[0] > time()) {
             return $cache[1];
         }
-        $result = $this->methodCallAsyncRead('channels.getSponsoredMessages', ['channel' => $peer]);
+        $result = $this->methodCallAsyncRead('messages.getSponsoredMessages', ['peer' => $peer]);
         if (\array_key_exists('messages', $result)) {
             $result = $result['messages'];
         } else {
@@ -67,6 +67,6 @@ trait Ads
         if (\is_array($message)) {
             $message = $message['random_id'];
         }
-        return $this->methodCallAsyncRead('channels.viewSponsoredMessage', ['channel' => $peer, 'random_id' => $message]);
+        return $this->methodCallAsyncRead('messages.viewSponsoredMessage', ['peer' => $peer, 'random_id' => $message]);
     }
 }
