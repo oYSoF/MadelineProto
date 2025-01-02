d236916739c9d77fd88ec8152cd8ba2b36ba75d1
Daniil Gentili <daniil@daniil.it>
2025-01-02 16:39:15 +0000

    Build for all arches with qemu

--- tests/docker.sh
+++ tests/docker.sh
@@ -8,25 +8,25 @@ docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
 
 has_arm=0
 for f in 192.168.1.2 192.168.69.4; do
-	if ping -c 1 $f; then
+	#if ping -c 1 $f; then
 		# Use emulation for everything
 		#docker buildx create --use --name wp --driver remote tcp://$f:1234
-		has_arm=1
-		break
-	fi
+		#has_arm=1
+		#break
+	#fi
 done
 
 has_riscv=0
-if ping -c 1 192.168.69.206; then
+#if ping -c 1 192.168.69.206; then
 	# Use emulation for everything
 	#docker buildx create --append --name wp --driver remote tcp://192.168.69.206:1234
-	has_riscv=1
-fi
+	#has_riscv=1
+#fi
 
 has_x86=0
 for f in 192.168.1.10 192.168.69.236 192.168.69.233 192.168.69.207 192.168.69.130; do
 	if ping -c 1 $f; then
-		docker buildx create --append --name wp --driver remote tcp://$f:1234
+		docker buildx create --use --name wp --driver remote tcp://$f:1234
 		has_x86=1
 		break
 	fi
